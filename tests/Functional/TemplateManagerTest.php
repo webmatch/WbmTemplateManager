9c109bfe1f1f73a43e6784fed5a4fc6fbb7e0a7e
neustadt <dneustadt@webmatch.de>
2017-04-04 16:47:08 +0200

    added functional tests

--- tests/Functional/TemplateManagerTest.php
+++ tests/Functional/TemplateManagerTest.php
@@ -7,4 +7,9 @@ class TemplateManagerTests extends Enlight_Components_Test_Controller_TestCase
         parent::setUp();
         $this->dispatch('/');
     }
+
+    public function testDummy()
+    {
+        $this->assertTrue(1 === 1);
+    }
 }
